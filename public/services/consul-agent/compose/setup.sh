19267a9117f3ac963c5b71ce2551f9efda77eb3c
Socheat <github@socheat.dev>
2026-03-31 17:32:14 +0700

    Implement support for compose override to "public/services/consul-agent/compose/setup.sh"

--- public/services/consul-agent/compose/setup.sh
+++ public/services/consul-agent/compose/setup.sh
@@ -2,9 +2,18 @@
 set -e
 export COMPOSE_PROJECT_NAME="consul"
 COMPOSE_FILE="https://soramitsukhmer-lab.github.io/services/consul-agent/compose/$(uname)/compose.yml"
+COMPOSE_FILE_OVERRIDE=""
 if [[ "$(uname)" == "Darwin" ]]; then
 	export HOSTNAME; HOSTNAME=$(scutil --get LocalHostName)
 else
 	export HOSTNAME; HOSTNAME=$(hostname)
 fi
-(set -x; curl -s "$COMPOSE_FILE" | docker compose -f - up -d --pull always)
+if [ -f "compose.override.yml" ]; then
+	COMPOSE_FILE_OVERRIDE="compose.override.yml"
+elif [ -f "docker-compose.override.yml" ]; then
+	COMPOSE_FILE_OVERRIDE="docker-compose.override.yml"
+fi
+if [ -n "$COMPOSE_FILE_OVERRIDE" ]; then
+	echo "+ Found override file: $COMPOSE_FILE_OVERRIDE"
+fi
+(set -x; curl -s "$COMPOSE_FILE" | docker compose -f - ${COMPOSE_FILE_OVERRIDE:+-f "$COMPOSE_FILE_OVERRIDE"} up -d --pull always)
