e3f904823c6b7dd3cf7f194ae28f768d7308221b
Socheat <github@socheat.dev>
2026-03-31 16:39:40 +0700

    Refactor consul-agent to use zeroconfig

--- public/services/consul-agent/compose/setup.sh
+++ public/services/consul-agent/compose/setup.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 set -e
-COMPOSE_FILE="https://soramitsukhmer-lab.github.io/services/consul-agent/compose/$(uname)/compose.yml"
-
-if [[ "$(uname)" == "Darwin" ]]; then
-  export HOSTNAME; HOSTNAME=$(scutil --get LocalHostName)
-else
-  export HOSTNAME; HOSTNAME=$(hostname)
-fi
-
 export COMPOSE_PROJECT_NAME="consul"
-export CONSUL_ADVERTISE_ADDRESS; CONSUL_ADVERTISE_ADDRESS=$(curl -s http://myip.sorakh.run)
-
+COMPOSE_FILE="https://soramitsukhmer-lab.github.io/services/consul-agent/compose/$(uname)/compose.yml"
+if [[ "$(uname)" == "Darwin" ]]; then
+	export HOSTNAME; HOSTNAME=$(scutil --get LocalHostName)
+else
+	export HOSTNAME; HOSTNAME=$(hostname)
+fi
 (set -x; curl -s "$COMPOSE_FILE" | docker compose -f - up -d --pull always)
